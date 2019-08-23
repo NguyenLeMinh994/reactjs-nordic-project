72c2b12ca0cc8c60f73b8e2b89424538fe2d3c12
NguyenLeMinh994 <kurosama99@gmail.com>
2019-08-23 23:41:28 +0700

    Updates

---
+++ precache-manifest.5c83077ecf7adeae310521a8c0716065.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5430db7bbfac2e96eb2930dd95887fe8",
+    "url": "/reactjs-nordic-project/index.html"
+  },
+  {
+    "revision": "efeb91b3d423dbfcb177",
+    "url": "/reactjs-nordic-project/static/css/main.b13421a6.chunk.css"
+  },
+  {
+    "revision": "35c5df36064a27288a60",
+    "url": "/reactjs-nordic-project/static/js/2.bd3bccd6.chunk.js"
+  },
+  {
+    "revision": "efeb91b3d423dbfcb177",
+    "url": "/reactjs-nordic-project/static/js/main.116c49c7.chunk.js"
+  },
+  {
+    "revision": "d2886ec221fc877ce41b",
+    "url": "/reactjs-nordic-project/static/js/runtime~main.5dbc2868.js"
+  }
+]);
